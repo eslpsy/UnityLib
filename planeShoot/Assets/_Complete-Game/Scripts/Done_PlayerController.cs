2ff6f84cb3b41010aa9dc77f97bab82906bdb747
eslpsy <zhangjianfeng415@gmail.com>
2017-11-02 23:26:10 +0800

    plane ctrl

--- planeShoot/Assets/_Complete-Game/Scripts/Done_PlayerController.cs
+++ planeShoot/Assets/_Complete-Game/Scripts/Done_PlayerController.cs
@@ -4,13 +4,13 @@ using System.Collections;
 [System.Serializable]
 public class Done_Boundary 
 {
-	public float xMin, xMax, zMin, zMax;
+	public float xMin, xMax, zMin, zMax;    //一次FixedUpdate移动的限定范围
 }
 
 public class Done_PlayerController : MonoBehaviour
 {
 	public float speed;                 // 玩家速度
-	public float tilt;                  // 左右平移偏转系数
+	public float tilt;                  // 左右旋转偏转系数
 	public Done_Boundary boundary;      
 
 	public GameObject shot;             // 子弹
@@ -26,30 +26,31 @@ public class Done_PlayerController : MonoBehaviour
 
     void Update ()
 	{
-		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		if (Input.GetButton("Fire1") && Time.time > nextFire)           // 按下"Fire1"按键，可以在project setting中自定义"Fire1"
 		{
 			nextFire = Time.time + fireRate;
-			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-			GetComponent<AudioSource>().Play ();
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);  // 产生一个子弹
+			GetComponent<AudioSource>().Play ();                        // 播放玩家攻击音效
 		}
 	}
 
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal");    //一次FixedUpdate水平偏移总值
+		float moveVertical = Input.GetAxis ("Vertical");        //一次FixedUpdate竖直偏移总值
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		GetComponent<Rigidbody>().velocity = movement * speed;
 		
-		GetComponent<Rigidbody>().position = new Vector3    
+		GetComponent<Rigidbody>().position = new Vector3    //移动到新的坐标
 		(
-            // 在min和max中获得一个随机值
+            // Mathf.Clamp(a, min, max)将a限定在min和max之间 a < min => a = min; a > max => a = max.
             Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
 			0.0f, 
 			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
 		);
 		
+        // 根据x方向移动的距离和系数tilt确定相对于z轴旋转的角度
 		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 	}
 }
